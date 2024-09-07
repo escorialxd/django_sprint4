cc03f6c8f911885919247d95651a6f202d230c02
escorialxd <escorialxd@gmail.com>
2024-09-07 13:11:03 +0300

    fix linter warnings in blog/views.py

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -145,7 +145,10 @@ class ProfileListView(PostsQuerySetMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["profile"] = get_object_or_404(User, username=self.kwargs["username"])
+        context["profile"] = get_object_or_404(
+            User,
+            username=self.kwargs["username"],
+        )
         return context
 
 
